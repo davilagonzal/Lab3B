f4d9a595c978895de94e0560dc78f43a3c98f9ef
davilagonzal <89435675+davilagonzal@users.noreply.github.com>
2022-02-22 14:14:57 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿{ 
+
+    float percentageMales;
+    float percentageFemales;
+    float maleFemale;
+
+    Console.WriteLine("Hello, how many female cats are there?");
+    int femaleCats = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("How many male cats are there?");
+    int maleCats = Convert.ToInt32(Console.ReadLine());
+
+// Equation to get prcentage
+    maleFemale = (maleCats + femaleCats);
+    percentageMales = (maleCats / maleFemale);
+    percentageFemales = (femaleCats / maleFemale);
+
+    System.Console.WriteLine("The precentage of male cats in the shelter is " + percentageMales);
+    System.Console.WriteLine("The percentage of female cats in the shelter is " + percentageFemales);
+}
